99e1a60f6e043e928ead7360212b9d750cf09af5
Danil Terentev <danil.terentyev@gmail.com>
2024-08-03 11:58:50 +0300

    Update module_6_3.py

--- module_6_3.py
+++ module_6_3.py
@@ -1,16 +1,18 @@
 class Horse:
+    sound = 'Frrr'  # Звук, который издает лошадь
+
     def __init__(self):
-        self.x_distance = 0  # Пройденный путь
-        self.sound = 'Frrr'  # Звук, который издает лошадь
+        self.x_distance = 0  # Начальная точка пути
 
     def run(self, dx):
         self.x_distance += dx  # Изменение пройденного пути на dx
 
 
 class Eagle:
+    sound = 'I train, eat, sleep, and repeat'  # Звук, который издает орел
+
     def __init__(self):
-        self.y_distance = 0  # Высота полета
-        self.sound = 'I train, eat, sleep, and repeat'  # Звук, который издает орел
+        self.y_distance = 0  # Начальная высота полета
 
     def fly(self, dy):
         self.y_distance += dy  # Изменение высоты полета на dy
@@ -42,3 +44,5 @@ p1.move(-5, 20)
 print(p1.get_pos())
 
 p1.voice()
+
+print(Pegasus.mro())
